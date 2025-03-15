ae804c3c3728a612f98aa209197ee46606b6cfcb
nathan122333 <Nathanngugi3@gmail.com>
2025-03-15 10:21:32 +0300

    Add files via upload

---
+++ tictacto.py
@@ -0,0 +1,99 @@
+positions={
+    "top-left":" ",
+    "top-mid":" ",
+    "top-right":" ",
+    "mid-left":" ",
+    "mid-mid":" ",
+    "mid-right":" ",
+    "bottom-left":" ",
+    "bottom-mid":" ",
+    "bottom-right":" ",
+}
+
+board= positions["top-left"]+"|"+positions["top-mid"]+"|"+positions["top-right"]
+boarder_bottom="-------"
+print(board)
+print(boarder_bottom)
+board= positions["mid-left"]+"|"+positions["mid-mid"]+"|"+positions["mid-right"]
+boarder_bottom="-------"
+print(board)
+print(boarder_bottom)
+board= positions["bottom-left"]+"|"+positions["bottom-mid"]+"|"+positions["bottom-right"]
+boarder_bottom="-------"
+print(board)
+print(boarder_bottom)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
